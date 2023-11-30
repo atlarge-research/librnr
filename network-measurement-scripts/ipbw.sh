0b1500e16f43fa939968f7533afc65a5cb0459ea
Jesse Donkervliet <j.donkervliet@gmail.com>
2023-11-30 07:02:44 -0500

    airplane network trace

--- network-measurement-scripts/ipbw.sh
+++ network-measurement-scripts/ipbw.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 # Download
-iperf3 -c al01.anac.cs.vu.nl -p 7979 -R --json >>./iperf_download_ams_connectivity.json
+IPERF3_PASSWORD=rockstar iperf3 -c al01.anac.cs.vu.nl -p 7979 -R --json --rsa-public-key-path iperf3_pub.pem --username jesse >>./iperf_download_ams.json
+
 # Upload
-iperf3 -c al01.anac.cs.vu.nl -p 7979 --json >>./iperf_upload_ams_connectivity.json
-# To test for yourself, start server using
-iperf3 -s -p 7979
+IPERF3_PASSWORD=rockstar iperf3 -c al01.anac.cs.vu.nl -p 7979 --json --rsa-public-key-path iperf3_pub.pem --username jesse >>./iperf_upload_ams.json
+
+# Download
+IPERF3_PASSWORD=rockstar iperf3 -c 146.190.134.21 -p 7979 -R --json --rsa-public-key-path iperf3_pub.pem --username jesse >>./iperf_download_sfo.json
+
+# Upload
+IPERF3_PASSWORD=rockstar iperf3 -c 146.190.134.21 -p 7979 --json --rsa-public-key-path iperf3_pub.pem --username jesse >>./iperf_upload_sfo.json
+
+# # To test for yourself, start server using
+# iperf3 -s -p 7979
--- network-measurement-scripts/measurebw.sh
+++ network-measurement-scripts/measurebw.sh
@@ -3,9 +3,23 @@
 chmod +x ./ipbw.sh
 chmod +x ./mbw.sh
 
+ping -i 1 1.1.1.1 >>./ping_1_1_1_1.log &
+PID_P1=$!
+
+ping -i 1 8.8.8.8 >>./ping_8_8_8_8.log &
+PID_P2=$!
+
+ping -i 1 al01.anac.cs.vu.nl >>./ping_ams.log &
+PID_P3=$!
+
+ping -i 1 146.190.134.21 >>./ping_sfo.log &
+PID_P4=$!
+
+trap 'kill $PID_P1 $PID_P2 $PID_P3 $PID_P4' EXIT
+
 while true; do
     date +%s >>./time.log
     ./ipbw.sh
-    ./mbw.sh
+    # ./mbw.sh
     sleep 60
 done
