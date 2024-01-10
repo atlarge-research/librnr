99dfef69608d3010a5304300abc58a3ae15af24a
Jesse <j.donkervliet@gmail.com>
2024-01-10 18:14:41 +0100

    feat: measure battery

---
+++ scripts/initmeasurebattery.ps1
@@ -0,0 +1,3 @@
+
+Write-Output "Run this script before enabling Quest Link"
+adb shell am start -n "com.example.batterymanager_utility/com.example.batterymanager_utility.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
--- scripts/installbench.ps1
+++ scripts/installbench.ps1
@@ -4,6 +4,14 @@ $rnrDirPath = "$env:LOCALAPPDATA\librnr"
 $Name = "$rnrDirPath\manifest.json"
 $rnrFilePath = "$rnrDirPath\rnr.dll"
 $Path = "HKLM:\SOFTWARE\Khronos\OpenXR\1\ApiLayers\Implicit"
+$ApkFilePath = "com.example.batterymanager_utility.apk"
+
+# Check that the headset is connected via ADB
+$confirmation = Read-Host "Is your VR device connected via ADB? [yn]"
+if ($confirmation -ne 'y') {
+    Write-Host "Please connect your VR device and try again."
+    exit 1
+}
 
 # Create or set registry key value to 0 to enable library
 if (!(Test-Path $Path)) {
@@ -21,5 +29,10 @@ Invoke-WebRequest -Uri "https://github.com/atlarge-research/librnr/releases/late
 # Download rnr.dll
 Write-Output "downloading library..."
 Invoke-WebRequest -Uri "https://github.com/atlarge-research/librnr/releases/latest/download/rnr.dll" -OutFile $rnrFilePath
+
+Write-Output "installing battery measurement APK..."
+Invoke-WebRequest -Uri "https://github.com/S2-group/batterymanager-companion/releases/latest/download/com.example.batterymanager_utility.apk" -OutFile $ApkFilePath
+adb install -g $ApkFilePath
+Remove-Item $ApkFilePath
 
 Write-Output "done!"
